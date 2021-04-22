b109b9b839638155c302b9b947c01b13ecf9b257
wcx410 <2570337109@qq.com>
2021-04-22 09:06:37 +0800

    img

--- src/main.js
+++ src/main.js
@@ -4,10 +4,11 @@ import App from './App.vue'
 //导入elementui
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import Axios from "axios"
 
 Vue.use(ElementUI)
 
-import Axios from "axios"
+
 
 Axios.defaults.baseURL="http://localhost:8090/xsyx/"
 
